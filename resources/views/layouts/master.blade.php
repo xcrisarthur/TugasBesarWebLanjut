0da45cfc6e617b7d057ee7a3758d79936bdb7a72
Bryan Matthews <90823109+Bluekollibri@users.noreply.github.com>
2023-06-27 19:32:19 +0700

    Add files via upload

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,50 +2,35 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<!-- Font Awesome Icons -->
-	<link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}">
-	<!-- Theme style -->
-	<link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
-	<!-- Google Font: Source Sans Pro -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <title>Login</title>
 
-	@yield('addCss')
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+    @yield('addCss')
 </head>
 
-<body class="hold-transition sidebar-mini sidebar-collapse">
-	<div class="wrapper">
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <h1>UKM</h1>
+        <h2>Universitas Kristen Maranatha</h2>
+    </div>
 
-		@include('layouts.navbar')
-		@include('layouts.sidebar')
-
-		<!-- Content Wrapper. Contains page content -->
-		<div class="content-wrapper">
-			@yield('content')
-		</div>
-		<!-- /.content-wrapper -->
-
-		@include('layouts.footer')
-	</div>
-	<!-- ./wrapper -->
-
-	<!-- REQUIRED SCRIPTS -->
-
-	<!-- jQuery -->
-	<script src="{{asset('js/jquery.min.js')}}"></script>
-	<!-- Bootstrap 4 -->
-	<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
-	<!-- AdminLTE App -->
-	<script src="{{asset('js/adminlte.min.js')}}"></script>
-	<!-- Sweetalert -->
-	<script src="{{asset('js/sweetalert.min.js')}}"></script>
-
-	@yield('addJavascript')
+    @yield('content')
+</div>
+<!-- /.login-box -->
 </body>
 
 </html>
